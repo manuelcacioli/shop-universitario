838ec95bab79710f57f1b06908f7a95fd9735b9a
manuelcacioli <manuelcacioli13@gmail.com>
2024-11-30 19:46:58 +0100

    2 push con prima versione di front end

--- shop/src/main/java/com/example/shop/controller/ProductController.java
+++ shop/src/main/java/com/example/shop/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.example.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +18,18 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @GetMapping("/list/product")
-    public ResponseEntity<List<ProductEntity>> getAllProducts() {
+//    @GetMapping("/list/product")
+//    public ResponseEntity<List<ProductEntity>> getAllProducts() {
+//        List<ProductEntity> products = service.getAllProduct();
+//        return ResponseEntity.ok(products);
+//
+//    }
+
+    @GetMapping("/list")
+    public String showProducts(Model model) {
         List<ProductEntity> products = service.getAllProduct();
-        return ResponseEntity.ok(products);
+        model.addAttribute("products", products); // Passa la lista prodotti alla vista
+        return "products"; // Nome del file HTML senza suffisso
     }
 
 }
